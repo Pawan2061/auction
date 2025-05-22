0a43e31b6320952edfbfe9acb6ee5b9ec0ea6f3a
dipenbhat557 <pandeypawan2061@gmail.com>
2025-05-22 23:00:38 +0530

    feat: stop trackking unnecessary files

--- backend/dist/db/seq.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_1 = require("sequelize");
-exports.sequelize = new sequelize_1.Sequelize("postgresql://postgres:Kr@ken2128@db.llmxqwikhthywcpyhahq.supabase.co:5432/postgres");
